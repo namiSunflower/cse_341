a77aa2694c139be8d9763f51ef88de996bf26029
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-06-12 00:37:15 +0800

    update

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-const distDir = __dirname + "/dist/";
 
 const port = process.env.PORT || 3000;
 const app = express();
 
-
-app.use(express.static(distDir))
+app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
