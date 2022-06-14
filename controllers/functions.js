c85d3c6088d86f9455e4921a4a6d1cd5c618e50c
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-06-15 05:05:09 +0800

    save

--- controllers/functions.js
+++ controllers/functions.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
@@ -22,5 +23,61 @@ const displayOne = async (req, res, next) => {
   });
 };
 
+const createOne = async (req, res, next) => {
+  const newContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  }
+  const result = await mongodb.getDb().db('cse341').collection('contacts').insertOne(newContact);
+  if (result.acknowledged){
+    res.status(201).json(response);
+  }
+  else{
+    res.status(500).json(result.error || 'An error occurred while creating the new contact.')
+  }
+};
 
-module.exports = {displayAll, displayOne};
+const updateOne = async (req, res, next) => {
+  const userId = new ObjectId(req.params.id);
+  //Be aware that using updateOne instead will update specific fields only
+  const updatedContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  }
+  const result = await mongodb
+    .getDb()
+    .db('cse341')
+    .collection('contacts')
+    .replaceOne({_id:userId},updatedContact);
+    console.log(result);
+    if(result.modifiedCount > 0){
+      res.status(204).send();
+    } else {
+      res.status(500).json(response.error || 'An error occurred while updating the contact.')
+    }
+}
+
+const deleteOne = async(req, res) => {
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+  .getDb()
+  .db('cse341')
+  .collection('contacts')
+  .remove({ _id: userId }, true);
+  if (result.deletedCount > 0){
+    res.status(204).send();
+  } else {
+    res.status(500).json(result.error || 'An error occurred while deleting the contact');
+  }
+}
+
+
+
+
+module.exports = {displayAll, displayOne, createOne, updateOne, deleteOne};
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,21 @@
 const routes = require('express').Router();
 const controller = require('../controllers/functions');
 
+//Read
 routes.get('/', controller.displayAll)
 
 routes.get('/:id', controller.displayOne);
+
+//Create
+routes.post('/', controller.createOne)
+
+//Update
+routes.put('/:id', controller.updateOne)
+
+//Delete
+routes.delete('/:id', controller.deleteOne)
+
+
 
 // routes.get('/', (req, res) => {
 //     res.send("Hi");
