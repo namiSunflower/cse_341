7ef4e035078957477c97aa235e5971a1f2d785a6
Frances Naomi Alido <55601810+namiSunflower@users.noreply.github.com>
2022-06-18 00:55:50 +0800

    updated

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: false }))
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   }).use(cors())
   .use('/', require('./routes'))
